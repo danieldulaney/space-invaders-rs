eaee6a7aa9bbea28772bafbf3a4733768185f1e2
Daniel Dulaney <dulaney.daniel@gmail.com>
2018-07-29 03:56:07 +0000

    Added utility fns, tests, and minor refactoring

--- src/asm.rs
+++ src/asm.rs
@@ -1,11 +1,13 @@
+// See http://www.nj7p.info/Manuals/PDFs/Intel/9800153B.pdf
+#[derive(Debug)]
 pub enum Instruction {
     // Page 4-4
     MOVRegReg { dest: Register, src: Register },
-    MOVMemReg { dest: Register, src: Address },
-    MOVRegMem { dest: Address, src: Register },
-    MVIReg { dest: Register, data: u8 },
-    MVIMem { data: u8 },
-    LXI { dest: Pair, data: u16 },
+    MOVMemReg { dest: Register },
+    MOVRegMem { src: Register },
+    MVIReg { dest: Register, src: u8 },
+    MVIMem { src: u8 },
+    LXI { dest: Pair, src: u16 },
 
     // Page 4-5
     LDA { src: Address },
@@ -93,13 +95,14 @@ pub enum Instruction {
     HLT,
     NOP,
 
-    Unknown(u8),
+    Unknown(Option<u8>),
 }
 
 pub type Address = u16;
 
 pub type Port = u8;
 
+#[derive(Debug, PartialEq, Eq)]
 pub enum Register {
     A,
     B,
@@ -110,6 +113,7 @@ pub enum Register {
     L,
 }
 
+#[derive(Debug, PartialEq, Eq)]
 pub enum Pair {
     B,
     D,
@@ -117,6 +121,7 @@ pub enum Pair {
     SP,
 }
 
+#[derive(Debug, PartialEq, Eq)]
 pub enum Condition {
     NZ,
     Z,
@@ -128,3 +133,71 @@ pub enum Condition {
     M,
 }
 
+impl Register {
+    /// Determine the register based on the three least-significant bits.
+    ///
+    /// All higher bits are ignored. Uses the code on page 4-3 of the manual.
+    ///
+    /// # Panics
+    ///
+    /// Panics on inputs with 6 (`0b110`) in the relevant bits.
+    pub fn from_code(code: u8) -> Register {
+        match code & 0x07 {
+            // 0b00000111
+            0x07 => Register::A,
+            0x00 => Register::B,
+            0x01 => Register::C,
+            0x02 => Register::D,
+            0x03 => Register::E,
+            0x04 => Register::H,
+            0x05 => Register::L,
+            _ => panic!("Bad register number: {}", code),
+        }
+    }
+}
+
+impl Pair {
+    /// Determine the register pair based on the two least-significant bits.
+    ///
+    /// All higher bits are ignored. Uses the code on page 4-3 of the manual.
+    pub fn from_code(code: u8) -> Pair {
+        match code & 0x03 {
+            // 0b00000011
+            0x00 => Pair::B,
+            0x01 => Pair::D,
+            0x02 => Pair::H,
+            0x03 => Pair::SP,
+            _ => unreachable!("Used masking to eliminate higher bits; this is impossible"),
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn correct_registers() {
+        assert_eq!(Register::from_code(0xFF), Register::A);
+        assert_eq!(Register::from_code(0), Register::B);
+        assert_eq!(Register::from_code(65), Register::C);
+        assert_eq!(Register::from_code(2), Register::D);
+        assert_eq!(Register::from_code(99), Register::E);
+        assert_eq!(Register::from_code(0xF4), Register::H);
+        assert_eq!(Register::from_code(0xAD), Register::L);
+    }
+
+    #[test]
+    #[should_panic]
+    fn panics_on_register_6() {
+        Register::from_code(70);
+    }
+
+    #[test]
+    fn correct_pairs() {
+        assert_eq!(Pair::from_code(0), Pair::B);
+        assert_eq!(Pair::from_code(97), Pair::D);
+        assert_eq!(Pair::from_code(26), Pair::H);
+        assert_eq!(Pair::from_code(0xFF), Pair::SP);
+    }
+}
